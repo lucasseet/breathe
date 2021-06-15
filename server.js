90a0ac36a29bd062cb2e01a49357575bb080d629
lucasseet <79414028+lucasseet@users.noreply.github.com>
2021-06-15 19:22:48 +0800

    modify port to use use env PORT when available

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const appRouter = require('./routers')
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; //tenary operator
 const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`
 
 mongoose.set('useFindAndModify', false)
